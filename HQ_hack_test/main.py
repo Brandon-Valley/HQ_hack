1159c5316271c0d0119b6025f8ecdf3750c716c5
Brandon Valley <bavvh8@mst.edu>
2019-03-26 12:55:06 -0500

    searches chrome

--- HQ_hack_test/main.py
+++ HQ_hack_test/main.py
@@ -1,3 +1,10 @@
-from google import google
-num_page = 3
-search_results = google.search("This is my query", num_page)
+import webbrowser
+search_terms = ['cats', 'dogs']
+
+# ... construct your list of search terms ...
+
+for term in search_terms:
+    url = "https://www.google.com.tr/search?q={}".format(term)
+#     webbrowser.open_new_tab(url)
+    chrome_browser = webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s")
+    chrome_browser.open_new_tab(url)
