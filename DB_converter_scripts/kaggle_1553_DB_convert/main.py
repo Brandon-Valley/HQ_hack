74e6bb891dad71d19ba417c05fbc5c2ac162cd01
Brandon Valley <bavvh8@mst.edu>
2019-03-26 22:16:18 -0500

    fixed below

--- DB_converter_scripts/kaggle_1553_DB_convert/main.py
+++ DB_converter_scripts/kaggle_1553_DB_convert/main.py
@@ -11,11 +11,11 @@ def read_text_file(file_path):
     
     
 #get rid of questions that dont have answers, q_d = question_dict
-def trimmed_input_data_d(raw_input_data_d):    
-    for q_d in raw_input_data_d:
+def trim_input_data_l(input_data_l):    
+    for q_d in input_data_l:
         if len(q_d['answers']) == 0:
-            raw_input_data_d.remove(q_d)
-    return raw_input_data_d
+            input_data_l.remove(q_d)
+#     return input_data_l
             
     
 # input_file_path = 'kaggle_1553_DB.json'
@@ -23,17 +23,17 @@ def trimmed_input_data_d(raw_input_data_d):
 # raw_input = read_text_file(input_file_path)
 
 with open('kaggle_1553_DB.json', 'r') as f:
-    raw_input_data_d = json.load(f)
+    input_data_l = json.load(f)
 
 # for distro in distros_dict:
 #     print(distro['answers'][0])
 
 
-trimmed_input_data_d(raw_input_data_d)
+trim_input_data_l(input_data_l)
 
 
 
-for distro in raw_input_data_d:
+for distro in input_data_l:
     print(distro['answers'][0])
 
 
