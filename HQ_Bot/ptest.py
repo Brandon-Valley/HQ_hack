ed898de8a757c993a8cc72953a7334a4c924e170
Brandon Valley <bavvh8@mst.edu>
2019-04-21 14:47:10 -0500

    can now open csv and still get stats

--- HQ_Bot/ptest.py
+++ HQ_Bot/ptest.py
@@ -1,12 +1,3 @@
-from nltk.corpus import wordnet as wn
-from itertools import product
+l = []
 
-wordx, wordy = "cat","dog"
-sem1, sem2 = wn.synsets(wordx), wn.synsets(wordy)
-
-maxscore = 0
-for i,j in list(product(*[sem1,sem2])):
-  score = i.wup_similarity(j) # Wu-Palmer Similarity
-  maxscore = score if maxscore < score else maxscore
-  
-print(maxscore)
+l[3] = 4
--- HQ_Bot/solving/solver_db_test_full.py
+++ HQ_Bot/solving/solver_db_test_full.py
@@ -1,5 +1,6 @@
 # to be able to import from parent dir
 import sys
+import autocorrect
 parent_dir_path = ''
 parent_dir_path_list = sys.path[0].split('\\')[0:-1]
 for dir in parent_dir_path_list:
@@ -42,6 +43,40 @@ import os
 
 
 
+def print_solved_csv_stats(solved_db_csv_path):
+    solved_db_dl = logger.readCSV(solved_db_csv_path)
+    
+    correct_answer_total = 0
+    wrong_answer_total   = 0
+    no_try_total         = 0
+    solver_times         = []
+    
+    for line_d in solved_db_dl:
+        solver_times.append(float(line_d['time']))
+        
+        if   line_d['right'] != '':
+            correct_answer_total += 1
+        elif line_d['wrong'] != '':
+            wrong_answer_total += 1
+        if line_d['tryed_to_solve'] != '':
+            no_try_total += 1
+            
+    result_percent_l = solver_utils.num_occurrence_percent_l([correct_answer_total, wrong_answer_total, no_try_total])
+    print('')
+    print('correct: ', result_percent_l[0] * 100)
+    print('wrong:   ', result_percent_l[1] * 100)
+    print('no_try:  ', result_percent_l[2] * 100)
+    print('')
+    print('avg solver time: ', (sum(solver_times) / len(solver_times)) )
+
+
+
+
+
+
+
+
+
 SOLVED_DB_HEADER_LIST = ['timestamp', 'question #', 'question', 'A', 'B', 'C', 'correct', 'wrong', 'tryed_to_solve', 'right', 'time' ]
 
 DB_CSV_PATH = 'test_db.csv'#"C:\\Users\\Brandon\\Documents\\Personal_Projects\\HQ_hack\\HQ_qo_database.csv"
@@ -55,7 +90,9 @@ try:
     os.remove(SOLVED_DB_CSV_PATH)
 except:
     print('making new solved db csv...')
-
+    
+    
+print('opening browser...')
 br = phantom_js_browser.Browser()
 keywords_d = utils.get_keywords_d_from_csv(KEYWORDS_CSV_PATH)
 
@@ -65,80 +102,84 @@ db_dl = logger.readCSV(DB_CSV_PATH)
 # print(db_dl)
 print('num lines to solve: ', len(db_dl) - 2)
 
-correct_answer_total = 0
-wrong_answer_total   = 0
-no_try_total         = 0
-solver_times         = []
+# correct_answer_total = 0
+# wrong_answer_total   = 0
+# no_try_total         = 0
+# solver_times         = []
 
-line_cnt = 0
-for line_d in db_dl:
-    question       = line_d['question']
-    options       = [line_d['A'],
-                     line_d['B'],
-                     line_d['C']]
-    correct_answer = line_d['correct']
-
-
-    if line_cnt != 0 and line_cnt % 180 == 0:
-        print ('sleeping...')
-        time.sleep(30)
-
-    solver_start_time = time.time()
+try:
     
-    solved_output_d = solver_select.solve(question, options, keywords_d, br)
-    
-    solver_end_time = time.time()    
-    solver_time = solver_end_time - solver_start_time
-    solver_times.append(solver_time)
+    line_cnt = 0
+    for line_d in db_dl:
+        question       = line_d['question']
+        options       = [line_d['A'],
+                         line_d['B'],
+                         line_d['C']]
+        correct_answer = line_d['correct']
     
     
-    tryed_to_solve = '' # empty box if it did try to solve
-    if solved_output_d['answer'] == None:
-        tryed_to_solve = 'NO_TRY'
-        no_try_total += 1
+    #     if line_cnt != 0 and line_cnt % 180 == 0:
+    #         print ('sleeping...')
+    #         time.sleep(30)
+    
+        solver_start_time = time.time()
         
-    wrong = ''
-    right = ''
-    if solved_output_d['answer'] != None:
-        if solved_output_d['answer'] == correct_answer:
-            right = 'RIGHT'
-            correct_answer_total += 1
-        else:
-            wrong = 'WRONG'
-            wrong_answer_total += 1
+        solved_output_d = solver_select.solve(question, options, keywords_d, br)
         
+        solver_end_time = time.time()    
+        solver_time = solver_end_time - solver_start_time
+    #     solver_times.append(solver_time)
+        
+        
+        tryed_to_solve = '' # empty box if it did try to solve
+        if solved_output_d['answer'] == None:
+            tryed_to_solve = 'NO_TRY'
+    #         no_try_total += 1
+            
+        wrong = ''
+        right = ''
+        if solved_output_d['answer'] != None:
+            if solved_output_d['answer'] == correct_answer:
+                right = 'RIGHT'
+    #             correct_answer_total += 1
+            else:
+                wrong = 'WRONG'
+    #             wrong_answer_total += 1
+            
+    
+        line_d['right'] = right
+        line_d['wrong'] = wrong
+        line_d['tryed_to_solve'] = tryed_to_solve
+        line_d['time'] = solver_time
+        
+        logger.logSingle(line_d, SOLVED_DB_CSV_PATH, wantBackup = False, headerList = SOLVED_DB_HEADER_LIST, overwriteAction = 'append')
+        
+        print('solved line:  %s,   time = %s' %(line_cnt + 2, solver_time))#`````````````````````````````````````````````````````````````````````````
+        line_cnt += 1
+except:
+    print('ERROR, %s is probably open' %(SOLVED_DB_CSV_PATH))
+    
+    
 
-    line_d['right'] = right
-    line_d['wrong'] = wrong
-    line_d['tryed_to_solve'] = tryed_to_solve
-    line_d['time'] = solver_time
-    
-    logger.logSingle(line_d, SOLVED_DB_CSV_PATH, wantBackup = False, headerList = SOLVED_DB_HEADER_LIST, overwriteAction = 'append')
-    
-    print('solved line:  %s,   time = %s' %(line_cnt + 2, solver_time))#`````````````````````````````````````````````````````````````````````````
-    line_cnt += 1
-    
-    
-# urllib.error.HTTPError: HTTP Error 503: Service Unavailable
-    
-    
-# for dict in db_dl:
-#     print('tryed_to_solve: ', dict['tryed_to_solve'])
-    
-# logger.logList(db_dl, SOLVED_DB_CSV_PATH, wantBackup = False, headerList = SOLVED_DB_HEADER_LIST, overwriteAction = 'overwrite')
-    
 
-result_percent_l = solver_utils.num_occurrence_percent_l([correct_answer_total, wrong_answer_total, no_try_total])
-print('correct: ', result_percent_l[0] * 100)
-print('wrong:   ', result_percent_l[1] * 100)
-print('no_try:  ', result_percent_l[2] * 100)
+
+# result_percent_l = solver_utils.num_occurrence_percent_l([correct_answer_total, wrong_answer_total, no_try_total])
+# print('correct: ', result_percent_l[0] * 100)
+# print('wrong:   ', result_percent_l[1] * 100)
+# print('no_try:  ', result_percent_l[2] * 100)
 
 end_time = time.time()
 print('total_time: ', end_time - start_time)
 
-print('avg solver time: ', sum(solver_times) / len(solver_times))
+# print('avg solver time: ', sum(solver_times) / len(solver_times))
 
 br.end()
+
+
+
+print_solved_csv_stats(SOLVED_DB_CSV_PATH)
+
+
 print('done!')
 
 
